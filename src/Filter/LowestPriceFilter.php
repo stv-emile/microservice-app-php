a918bb441021c08dcabe3751685eb9913e8a11c1
stv-emile <steevensedu@gmail.com>
2023-05-08 18:23:28 +0200

    [feat] filte: integrate modifyer price for product.

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -11,7 +11,24 @@ class LowestPriceFilter implements PromotionsFilterInterface
     public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion)
     : PromotionEnquiryInterface
     {
-        $enquiry->setDiscountedPrice(50);
+        $price = $enquiry->getProduct()->getPrice();
+        $quantity = $enquiry->getQuantity();
+        $lowestPrice = $quantity*$price;
+
+        //loop over promotions' modification logic against the enquiry
+        //1- check does the promotion apply
+        //2- apply the price modification to obtain a modified price (how)
+
+
+        //$modifiedPrice = $priceModifier->modify($price, $quantity, $promotion, $enquiry);
+
+
+        //3- check if $modifiedPrice < lowestProce
+            //1-save the enquiry properties
+            //2-update $lowestProice
+
+
+        $enquiry->setDiscountedPrice(250);
         $enquiry->setPrice(100);
         $enquiry->setPromotionId(3);
         $enquiry->setPromotionName('Black friday half price');
