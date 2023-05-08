5da7219e2b87d2d1365eb6b9c75ed2dc97c5a0f4
stv-emile <steevensedu@gmail.com>
2023-05-08 12:29:52 +0200

    [test] serviceTestCase: create service test case.

---
+++ tests/ServiceTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ServiceTestCase extends WebTestCase
+{
+    protected ContainerInterface $container;
+
+}
