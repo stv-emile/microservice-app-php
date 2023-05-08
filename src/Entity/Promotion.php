5658c5a9c558f91e258e6a00ce956aaed82c4913
stv-emile <steevensedu@gmail.com>
2023-05-08 02:20:13 +0200

    [fix] Entity: add ProductPromotion to PromotionEntity.

--- src/Entity/Promotion.php
+++ src/Entity/Promotion.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PromotionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PromotionRepository::class)]
@@ -24,6 +26,14 @@ class Promotion
 
     #[ORM\Column]
     private array $criteria = [];
+
+    #[ORM\OneToMany(mappedBy: 'promotion', targetEntity: ProductPromotion::class)]
+    private Collection $productPromotions;
+
+    public function __construct()
+    {
+        $this->productPromotions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -77,4 +87,22 @@ class Promotion
 
         return $this;
     }
+
+    /**
+     * @return Collection
+     */
+    public function getProductPromotions(): Collection
+    {
+        return $this->productPromotions;
+    }
+
+    /**
+     * @param Collection $productPromotions
+     */
+    public function setProductPromotions(Collection $productPromotions): void
+    {
+        $this->productPromotions = $productPromotions;
+    }
+
+
 }
